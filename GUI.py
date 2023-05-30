e12c4a28e5e045ee370136e551853558ada1a66c
Ruoqi Huang <huangruoqi374@gmail.com>
2023-05-30 16:42:19 -0700

    fix path

--- GUI.py
+++ GUI.py
@@ -1,5 +1,5 @@
 from UI_BASE.UI.app import App
-from scenes.select_scene import SelectScene as S1
+from src.scenes.select_scene import SelectScene 
 from src.scenes.labeling_scene import LabelingScene
 from src.scenes.setting_scene import SettingScene
 from src.vutils import load_settings
@@ -9,7 +9,7 @@ settings = load_settings()
 
 app = App(
     [
-        S1,
+        SelectScene,
         LabelingScene,
         SettingScene,
     ],
